f0285a989103809f721f53e8f92ba72c34b96c43
Francis Zhang <z527158571@gmail.com>
2025-09-29 15:03:32 +0800

    update app described

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ const App = () => {
     <GameProvider>
       <div className="app-frame">
         <header className="app-header">
-          <h1>扫雷</h1>
-          <p>一款界面优美的离线扫雷游戏</p>
+          <h1>ClearMines</h1>
+          <p>一款基于 React 的离线扫雷游戏</p>
         </header>
         <GameShell />
       </div>
