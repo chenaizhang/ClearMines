f6f62f66cee7bac4c08b9bc5b17f340ea0604b7a
Francis Zhang <z527158571@gmail.com>
2025-09-30 00:01:50 +0800

    fix a bug：logo不显示

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,11 @@ const App = () => {
       <div className="app-frame">
         <header className="app-header">
           <div className="app-title">
-            <img className="app-logo" src="/image/logo.png" alt="ClearMines logo" />
+            <img
+              className="app-logo"
+              src="./image/logo.png"
+              alt="ClearMines logo"
+            />
             <h1>ClearMines</h1>
           </div>
           <p>一款基于 React 的离线扫雷游戏</p>
